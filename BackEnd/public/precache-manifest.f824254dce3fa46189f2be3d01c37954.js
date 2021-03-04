0b307aebcdfcb41b87bdb88dfb58380fcd8921b2
Abhinav-Taneja <abhinavtaneja00@gmail.com>
2021-03-04 13:24:27 +0530

    Mern-Project

---
+++ BackEnd/public/precache-manifest.f824254dce3fa46189f2be3d01c37954.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46bd2aca4f755f6e233fa58614f06e45",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10e5c4d0de5b7025498b",
+    "url": "/static/css/0.bbaacb0d.chunk.css"
+  },
+  {
+    "revision": "c1bed28c69dc51c3efc9",
+    "url": "/static/css/4.c04b0b52.chunk.css"
+  },
+  {
+    "revision": "bd344c914e6384d79eac",
+    "url": "/static/css/5.f3505a8f.chunk.css"
+  },
+  {
+    "revision": "82efbb1a895dab058f4b",
+    "url": "/static/css/6.93f3b060.chunk.css"
+  },
+  {
+    "revision": "447b7c4647db84bd3755",
+    "url": "/static/css/7.6cafb8e4.chunk.css"
+  },
+  {
+    "revision": "9862ba8b06e37c07fac6",
+    "url": "/static/css/8.4c388a3f.chunk.css"
+  },
+  {
+    "revision": "063167b6e85fcb161bbf",
+    "url": "/static/css/main.08b7a5b4.chunk.css"
+  },
+  {
+    "revision": "10e5c4d0de5b7025498b",
+    "url": "/static/js/0.96070edd.chunk.js"
+  },
+  {
+    "revision": "fc2098d8696f6dc6d8ba",
+    "url": "/static/js/3.9eb3540a.chunk.js"
+  },
+  {
+    "revision": "c1bed28c69dc51c3efc9",
+    "url": "/static/js/4.87900152.chunk.js"
+  },
+  {
+    "revision": "bd344c914e6384d79eac",
+    "url": "/static/js/5.cd60f380.chunk.js"
+  },
+  {
+    "revision": "82efbb1a895dab058f4b",
+    "url": "/static/js/6.90a4938f.chunk.js"
+  },
+  {
+    "revision": "447b7c4647db84bd3755",
+    "url": "/static/js/7.2ca6efdb.chunk.js"
+  },
+  {
+    "revision": "9862ba8b06e37c07fac6",
+    "url": "/static/js/8.59667290.chunk.js"
+  },
+  {
+    "revision": "063167b6e85fcb161bbf",
+    "url": "/static/js/main.f96ecbb2.chunk.js"
+  },
+  {
+    "revision": "57500dadd0d6f730f885",
+    "url": "/static/js/runtime-main.94c25952.js"
+  }
+]);
